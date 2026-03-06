2d42d8bd4e6173d1d8e4f69ffd06df03f6272265
Luís Filipe da Costa Caldeira <luis.caldeira@gmail.com>
2026-03-06 23:12:02 +0000

    added urls

--- apps/MapsBORA/urls.py
+++ apps/MapsBORA/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.submission_list, name="submission_list"),
+    #path("", views.submission_list, name="submission_list"),
+    path('', views.index, name='index'),
 ]
