904f38066474319ad62dc29a0f2ae0ea9db6a658
Luís Filipe da Costa Caldeira <luis.caldeira@gmail.com>
2026-03-06 15:13:41 +0000

    correcting model import in admin

--- apps/MapsBORA/admin.py
+++ apps/MapsBORA/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import MapSubmission
+from .models import Submissao
 
-admin.site.register(apps.MapSubmission)
+admin.site.register(Submissao)
