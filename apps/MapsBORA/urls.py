f9cf38cb88fd73965a13d6eb74aa2bc8e0db6b25
Luís Filipe da Costa Caldeira <luis.caldeira@gmail.com>
2026-03-07 10:08:49 +0000

    changes in url and index

--- apps/MapsBORA/urls.py
+++ apps/MapsBORA/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     #path("", views.submission_list, name="submission_list"),
     path('', views.index, name='index'),
     path('edit/<int:id>/', views.edit_submission, name='edit_submission'),
+    path('approve/<int:id>/', views.approve_submission, name='approve_submission'),
+    path('delete/<int:id>/', views.delete_submission, name='delete_submission'),
+    path('recover/<int:id>/', views.recover_token, name='recover_token'),
 ]
