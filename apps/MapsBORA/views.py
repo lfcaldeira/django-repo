51ec3d8b11b0823defa4575f302e254a51b6108c
Luís Filipe da Costa Caldeira <luis.caldeira@gmail.com>
2026-03-07 09:17:18 +0000

    debugging

--- apps/MapsBORA/views.py
+++ apps/MapsBORA/views.py
@@ -40,6 +40,7 @@ def edit_submission(request, id):
 
     if request.method == 'POST':
         word = request.POST.get('randomword')
+        messages.success(request,"this is the token"+randomword)
 
         if word == submission.randomword:
             submission.mapper_name = request.POST.get('mapper_name')
