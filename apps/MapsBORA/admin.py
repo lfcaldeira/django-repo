b748bc259da6a4b4112adf4efb9f2958df1fa479
Luís Filipe da Costa Caldeira <luis.caldeira@gmail.com>
2026-03-09 21:33:58 +0000

    code adjustment.

--- apps/MapsBORA/admin.py
+++ apps/MapsBORA/admin.py
@@ -29,17 +29,10 @@ def export_as_csv(modeladmin, request, queryset):
     return response
 
 @admin.register(Submission)
-class RideAdmin(admin.ModelAdmin):
-    list_display = ('map_name', 'request_date', 'status', 'mapper_name')
-    list_filter = ('status', 'request_date')
-    actions = [export_as_csv]
-
 class SubmissionAdmin(admin.ModelAdmin):
     list_display = ('map_name', 'request_date', 'status', 'token','mapper_name','map_url')
     list_filter = ('status', 'request_date','map_url')
     search_fields = ('map_name', 'mapper_name','status','map_url')
     date_hierarchy = ('request_date')
     list_editable = ('status',)
-
-admin.site.register(Submission, SubmissionAdmin)
 
